b3af9ca98dab1d15dfe1eea4a212de9a54f465b2
Michal Čaniga <michalcaniga559@gmail.com>
2020-05-22 22:11:26 +0200

    interfaces prefixes (#13)

--- src/client/src/interfaces/entities/Project.ts
+++ src/client/src/interfaces/entities/Project.ts
@@ -1,11 +1,11 @@
-import {User} from "./User";
-import {Work} from "./Work";
+import {IUser} from "./User";
+import {IWork} from "./Work";
 
-export interface Project {
+export interface IProject {
     id: number;
     name: string;
     created: Date;
-    owner: User;
-    invitedUsers: User[];
-    works: Work[];
+    owner: IUser;
+    invitedUsers: IUser[];
+    works: IWork[];
 }
